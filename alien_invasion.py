bc6da032196e38e340595f2e39e346388e577f26
khrystyna <shupa@tuta.io>
2020-11-11 20:41:26 -0600

    Added full screen mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,8 +10,9 @@ class AlienInvasion:
         """Initialize the game and create game resourses."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
